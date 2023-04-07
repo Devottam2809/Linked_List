d7aecfe220295303027001fd7dc4c11146846877
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-04-07 18:05:40 +0530

    Create Delete_end.cpp

---
+++ Delete_end.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+struct node{
+    int data;
+    node *next;
+};
+
+node *createnode(node *root)
+{
+    int data;
+    cout<<"Enter the data : ";
+    cin>>data;
+
+    node *p=(node *)malloc(sizeof(node));
+    p->data=data;
+    p->next=NULL;
+    node *ptr;
+
+    if(root==NULL)
+    {
+        root=p;
+        ptr=root;
+    return root;
+    }
+    ptr->next=p;
+    ptr=p;
+return root;
+}
+
+void print_list(node *root)
+{
+    node *current=root;
+    while(current!=NULL)
+    {
+        cout<<current->data<<" ";
+        current=current->next;
+    }
+}
+
+void delete_end(node *root)
+{
+    node *current=root;
+    node *temp=NULL;
+    while(current->next!=NULL)
+    {
+        temp=current;
+        current=current->next;
+    }
+    temp->next=NULL;
+    free(temp);
+    print_list(root);
+}
+
+int main()
+{
+    node *root=NULL;
+    int n;
+    cout<<"Enter the number of nodes : ";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+        root=createnode(root);
+    
+    print_list(root);
+    cout<<endl;
+    delete_end(root);
+    
+    return 0;
+}
